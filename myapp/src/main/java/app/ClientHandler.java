2e2d5cfe85166ddedae531640eefb527d3699231
HarpreetSingh1603 <harpreet.singh@iisvittorioveneto.it>
2021-12-05 17:52:34 +0100

    Invio HTML e CSS funzionante manca immagine

--- myapp/src/main/java/app/ClientHandler.java
+++ myapp/src/main/java/app/ClientHandler.java
@@ -1,12 +1,14 @@
 package app;
 
+import java.awt.image.BufferedImage;
 import java.net.*;
 import java.io.*;
+import javax.imageio.ImageIO;
 
 public class ClientHandler extends Thread {
-    PrintWriter outVersoClient;
     ServerSocket server;
     BufferedReader inDalClient;
+    OutputStream os;
     String request;
     String resource;
     String badResource;
@@ -30,32 +32,66 @@ public class ClientHandler extends Thread {
 
     private void comunica() throws Exception {
         
-        inDalClient = new BufferedReader(new InputStreamReader(client.getInputStream()));
-        outVersoClient = new PrintWriter(client.getOutputStream(), true);
+        BufferedImage bImage = ImageIO.read(new File("/home/studente/Desktop/singh_tipsit/myapp/htdocs/immagine.jpg"));
+        ByteArrayOutputStream bos = new ByteArrayOutputStream();
+        ImageIO.write(bImage, "jpg", bos );
+        byte [] data = bos.toByteArray();
         
+        inDalClient = new BufferedReader(new InputStreamReader(client.getInputStream()));
+
+
+        String httpHeader;
+        os = client.getOutputStream();
         
         
         request = inDalClient.readLine();
         
-        resource = "<!DOCTYPE html>\n<html><body><h1>Risorsa</h1></body></html>";
-        badResource = "<!DOCTYPE html>\n<html><body><h1>errore</h1></body></html>";
+        System.out.println(request);
         
-        if(request.contains("GET")){
-            outVersoClient.println("HTTP/1.1 200 OK");
-            outVersoClient.println("Content-Type: text/html");
-            outVersoClient.println("Content-Lenght: " + resource.length());
-            outVersoClient.println("");
-            outVersoClient.println(resource);            
-        } else {
-            outVersoClient.println("HTTP/1.1 400 Bad Request");
-            outVersoClient.println("Content-Type: text/html");
-            outVersoClient.println("Content-Lenght: " + badResource.length());
-            outVersoClient.println("");
-            outVersoClient.println(badResource);
+        resource = "<!DOCTYPE html><html><head><meta charset=\"UTF-8\"><link rel=\"stylesheet\" href=\"style.css\"><title>Index</title></head><body><h1>Index</h1><p>Indice dei contenuti</p><ul><li>Link al file <a href=\"immagine.jpg\">immagine.jpg</a></li><li>Link al documento <a href=\"documento.pdf\">documento.pdf</a></li></ul><p>Immagine:</p><img src=\"immagine.jpg\" alt=\"natura\" /></body></html>";
+        
+        
+        if (request.contains("GET / HTTP/1.1")) {
+            httpHeader
+                    = "HTTP/1.1 200 OK\n\r"
+                    + "Content-Type: text/html\n\r"
+                    + "Content-Lenght: " + resource.length()
+                    + "\n\r"
+                    + "\n\r"
+                    + resource;
+
+            os.write(httpHeader.getBytes(), 0, httpHeader.length());
+            os.flush();
         }
-        
-        //outVersoClient.println("Funziona!");
-        
+        if(request.contains("style.css")){
+            resource = "body {background-color: #fffdf0;color: #600;}";
+            System.out.println("Test");
+            httpHeader
+                    = "HTTP/1.1 200 OK\n\r"
+                    + "Content-Type: text/css\n\r"
+                    + "Content-Lenght: " + resource.length()
+                    + "\n\r"
+                    + "\n\r"
+                    + resource;
+
+            os.write(httpHeader.getBytes(), 0, httpHeader.length());
+            os.flush();
+        }
+        if(request.contains("immagine.jpg")){
+            
+            System.out.println("Test");
+            httpHeader
+                    = "HTTP/1.1 200 OK\n\r"
+                    + "Content-Type: image/jpg;Base64\n\r"
+                    + "Content-Lenght: " + data.length
+                    + "\n\r"
+                    + "\n\r"
+                    + data;
+
+
+            os.write(httpHeader.getBytes(), 0, httpHeader.length());
+            os.flush();
+        }
         
         client.close();
 
